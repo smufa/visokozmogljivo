78c519524ddadc9e80db16f682852143349e6ce9
Lan Vukušič <lan.vukusic@gmail.com>
2025-03-22 12:32:56 +0100

    paralel seam carving, upwards triangles working

--- naloga1/main.cpp
+++ naloga1/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include "Image.hpp"
 #include "sequential.hpp"
+#include "parallel.hpp"
 
 // Carve function now uses the Image class
-Image carve(const Image& img, int pixels_to_remove) {
+Image carve(const Image &img, int pixels_to_remove)
+{
     auto current = img;
-    for(int i = 0; i < pixels_to_remove; i++) {
-        auto seams = id_seams_seq(calc_energy_seq(current));
+    for (int i = 0; i < pixels_to_remove; i++)
+    {
+        auto energy = calc_energy_seq(current);
+        auto seams = id_seams_par(energy);
+        seams.save("testout.png");
         current = rem_seam_seq(current, seams);
     }
     return current;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
         std::cerr << "Usage: " << argv[0] << " <input> <output> <pixels_to_remove>\n";
         return EXIT_FAILURE;
     }
@@ -22,20 +29,25 @@ int main(int argc, char* argv[]) {
     std::string input_filename = argv[1];
     std::string output_filename = argv[2];
     int pixels_to_remove;
-    
-    try {
+
+    try
+    {
         pixels_to_remove = std::stoi(argv[3]);
-    } catch (...) {
+    }
+    catch (...)
+    {
         std::cerr << "Invalid pixel count\n";
         return EXIT_FAILURE;
     }
 
-    try {
+    try
+    {
         // Load image
         Image input = Image::load(input_filename);
 
-        if (pixels_to_remove <= 0 || pixels_to_remove >= input.getWidth()) {
-            std::cerr << "Invalid removal value (must be 1-" << input.getWidth()-1 << ")\n";
+        if (pixels_to_remove <= 0 || pixels_to_remove >= input.getWidth())
+        {
+            std::cerr << "Invalid removal value (must be 1-" << input.getWidth() - 1 << ")\n";
             return EXIT_FAILURE;
         }
 
@@ -43,12 +55,14 @@ int main(int argc, char* argv[]) {
         Image output = carve(input, pixels_to_remove);
 
         // Save result
-        if (!output.save(output_filename)) {
+        if (!output.save(output_filename))
+        {
             std::cerr << "Error saving image\n";
             return EXIT_FAILURE;
         }
     }
-    catch (const std::exception& e) {
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
--- naloga1/parallel.hpp
+++ naloga1/parallel.hpp
@@ -3,14 +3,18 @@
 #include "Image.hpp"
 #include <omp.h>
 
-inline Image calc_energy_par(Image in) {
+inline Image calc_energy_par(Image in)
+{
   Image out(in.getWidth(), in.getHeight(), 1);
-  
-  #pragma omp parallel for collapse(2) schedule(dynamic)
-  for (int x = 0; x < in.getWidth(); x++) {
-    for (int y = 0; y < in.getHeight(); y++) {
+
+#pragma omp parallel for collapse(2) schedule(dynamic)
+  for (int x = 0; x < in.getWidth(); x++)
+  {
+    for (int y = 0; y < in.getHeight(); y++)
+    {
       std::vector<float> energies = {0, 0, 0};
-      for (int c = 0; c < in.getChannels(); c++) {
+      for (int c = 0; c < in.getChannels(); c++)
+      {
         energies[c] = std::sqrt(
             std::pow(-in.at(c, x - 1, y - 1) - 2 * in.at(c, x, y - 1) -
                          in.at(c, x + 1, y - 1) + in.at(c, x - 1, y + 1) +
@@ -27,49 +31,120 @@ inline Image calc_energy_par(Image in) {
   return out;
 }
 
-inline Image id_seams_par(Image energy) {
-  const thr = omp_get_max_threads()
-  Image out(in.getWidth() - 1, in.getHeight(), in.getChannels());
+inline Image id_seams_par(Image energy)
+{
+  Image out(energy.getWidth(), energy.getHeight(), energy.getChannels());
+  const int num_triangles = 2;
+  const int strip_size = (out.getWidth() / num_triangles) / 2; // half of full traingle base
+  const int strips = energy.getHeight() / strip_size;
+  printf("strip size: %d, strips: %d\n", strip_size, strips);
 
+  // divide the image in (NON)equal horizontal strips
+  for (int strip = 0; strip * strip_size < out.getHeight(); strip++)
+  {
+    // calculate y boundaries for strips processing currently
+    const int strip_from = strip * strip_size;
+    const int strip_to = std::min(strip_from + strip_size, out.getHeight());
+    printf("strip from: %d, to: %d \n", strip_from, strip_to);
 
-  for(int t=0; t < thr; t++){
-    // each thread should compute the triangle
-    const triangle_width = out.getWidth() / 
+    for (int triangle_index = 0; triangle_index < num_triangles; triangle_index++)
+    {
+      // each thread should compute the triangle
+      // compute the triangle untill the width at the top is 0
+      int max_triangle_width = out.getWidth() / num_triangles;
+      int triangle_width = out.getWidth() / num_triangles;
+      int level = strip_from;
+
+      while (triangle_width > 0) // decrease triangle width by 2 every time you go up in Y
+      {
+        // from / to in x axis
+        const int from = (max_triangle_width * triangle_index + level - strip_from);
+        const int to = std::min(from + triangle_width, out.getWidth());
+        for (int i = from; i < to; i++)
+        {
+          const int prev_layer = out.getHeight() - level - 1;
+          const float min_previous_layer = std::min(
+              std::min(
+                  energy.at(0, i - 1, prev_layer),
+                  energy.at(0, i, prev_layer)),
+              energy.at(0, i + 1, prev_layer));
+
+          out.set(0, i, prev_layer + 1, 255);
+          // out.set(0, i, prev_layer + 1, min_previous_layer + energy.at(0, i, prev_layer + 1));
+        }
+        level += 1;
+        triangle_width -= 2;
+      }
+    }
+    // up facing triangles are now computed
+
+    // barier ? all threads have computed all the triangles in a strip
+
+    // // compute down facing triangles
+    // for (int triangle_index = 0; triangle_index < num_triangles; triangle_index++)
+    // {
+    //   // each thread should compute the triangle
+    //   // compute the triangle untill the width at the top is 0
+    //   int triangle_width = 2;
+    //   int level = strip_from + 1; // since the bottom layer was computed by up-facing triangle; +1 is the new level we compute
+    //   const int max_triangle_width = out.getWidth() / num_triangles;
+
+    //   while (triangle_width < max_triangle_width) // increase the triangle size by 2, building upwards.
+    //   {
+    //     // from / to in x axis
+    //     int from = triangle_index * max_triangle_width - level;         // calculate the from index (negative for first triangle)
+    //     const int to = std::max(from + triangle_width, out.getWidth()); // calculate real to
+    //     from = std::min(from, 0);                                       // correct the from, to work on first triangle too
+
+    //     for (int i = from; i < to; i++)
+    //     {
+    //       const int prev_layer = out.getHeight() - level - 1;
+    //       const float min_previous_layer = std::min(
+    //           std::min(
+    //               energy.at(0, i - 1, prev_layer),
+    //               energy.at(0, i, prev_layer)),
+    //           energy.at(0, i + 1, prev_layer));
+
+    //       out.set(0, i, prev_layer + 1, min_previous_layer + energy.at(0, i, prev_layer + 1));
+    //     }
+    //     level += 1;
+    //     triangle_width += 2;
+    //   }
+    // }
   }
 
+  //   float seam_score = std::numeric_limits<float>::infinity();
+  //   for(int x = 0; x < in.getWidth(); x++) {
+  //       if(seams.at(0, x, in.getHeight() - 1) < seam_score) {
+  //           seam_score = seams.at(0, x, in.getHeight() - 1);
+  //           seam_index = x;
+  //       }
+  //   }
 
-
-//   float seam_score = std::numeric_limits<float>::infinity();
-//   for(int x = 0; x < in.getWidth(); x++) {
-//       if(seams.at(0, x, in.getHeight() - 1) < seam_score) {
-//           seam_score = seams.at(0, x, in.getHeight() - 1);
-//           seam_index = x;
-//       }
-//   }
-
-//   for(int y = in.getHeight() - 2; y > 0; y--) {
-//       bool skipped = false;
-//       for(int x = 0; x < out.getWidth(); x++) {
-//           if(!skipped && x == seam_index) {
-//               if(seams.at(0, x-1, y-1) < seams.at(0, x, y-1) && seams.at(0, x-1, y-1) < seams.at(0, x+1, y-1)) {
-//                   seam_index = x - 1;
-//               } else if (seams.at(0, x, y-1) < seams.at(0, x+1, y-1)) {
-//                   seam_index = x;
-//               } else {
-//                   seam_index = x + 1;
-//               }
-//               skipped = true;
-//           } else {
-//               for(int c = 0; c < out.getChannels(); c++) {
-//                   out.set(c, skipped ? x-1 : x, y, in.at(c, x, y));
-//               }
-//           }
-//       }
-//   }
+  //   for(int y = in.getHeight() - 2; y > 0; y--) {
+  //       bool skipped = false;
+  //       for(int x = 0; x < out.getWidth(); x++) {
+  //           if(!skipped && x == seam_index) {
+  //               if(seams.at(0, x-1, y-1) < seams.at(0, x, y-1) && seams.at(0, x-1, y-1) < seams.at(0, x+1, y-1)) {
+  //                   seam_index = x - 1;
+  //               } else if (seams.at(0, x, y-1) < seams.at(0, x+1, y-1)) {
+  //                   seam_index = x;
+  //               } else {
+  //                   seam_index = x + 1;
+  //               }
+  //               skipped = true;
+  //           } else {
+  //               for(int c = 0; c < out.getChannels(); c++) {
+  //                   out.set(c, skipped ? x-1 : x, y, in.at(c, x, y));
+  //               }
+  //           }
+  //       }
+  //   }
   return out;
 }
 
-inline Image rem_seam_par(Image in, Image seams) {
-
+inline Image rem_seam_par(Image in, Image seams)
+{
+  return in;
 }
 #endif // PARALLEL_HPP
