f05ae54ad9ec79feb9fde932eef2fab0d6e561af
Lan Vukušič <lan.vukusic@gmail.com>
2025-03-22 16:02:24 +0100

    paralel seam calculation works

--- naloga1/main.cpp
+++ naloga1/main.cpp
@@ -14,9 +14,9 @@ Image carve(const Image &img, int pixels_to_remove)
         seams.normalize();
         seams.save("testout.png");
 
-        auto seams2 = id_seams_seq(energy);
-        seams2.normalize();
-        seams2.save("testout2.png");
+        // auto seams2 = id_seams_seq(energy);
+        // seams2.normalize();
+        // seams2.save("testout2.png");
         current = rem_seam_seq(current, seams);
     }
     return current;
